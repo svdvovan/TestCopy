a95ff8c3507301e4a248bc8e51c09657a62641f0
SretenskyVD <svdvovan@gmail.com>
2018-07-04 17:09:43 +0300

    запись в файл пробую

---
+++ src/TestTest/AllOpenCart.java
@@ -0,0 +1,59 @@
+package TestTest;
+
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.*;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.*;
+import java.util.Iterator;
+
+/**
+ * Created by SretenskyVD on 04.07.2018.
+ */
+public class AllOpenCart {
+    public static void main(String[] args) throws IOException {
+        String Path = "http://www.cmsmagazine.ru/catalogue/opencart-cms/works/";
+
+        int Page = 1;
+           for (int count = 0; count <= 1; count++) {
+            Path = "http://www.cmsmagazine.ru/catalogue/opencart-cms/works/?pn=" + Page;
+               System.out.println(Path);
+            //       String Path = "http://www.cmsmagazine.ru/catalogue/opencart-cms/works/?pn=all";
+
+
+            Document doc1 = (Document) Jsoup.connect(Path).get();
+
+            Elements links1 = doc1.getElementsByClass("fl expIconYesNo");
+
+               int y = 0;
+               for (Element link1 : links1) {
+                   String addressUrl = (links1.get(y).select("a").attr("_href"));
+                   System.out.println(addressUrl);
+
+//            Element table = doc1.getElementsByTag("table").get(8);
+//            Elements row = table.select("tr");
+//            Iterator<Element> ite = table.select("td").iterator();
+//
+//
+//            for (Element rows : row) {
+//                String Site = ite.next().select("a").attr("_href");
+//
+//                System.out.println(Site);
+//            }
+
+
+//            }
+//
+                   y++;
+               }
+               Page++;
+        }
+
+        }
+
+        }
+
+
